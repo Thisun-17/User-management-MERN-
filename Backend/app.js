71e6890098fe98dc173c124706ed3bee0a90854e
Thisun-17 <thisun.20233173@iit.ac.lk>
2025-05-27 01:31:56 +0530

    Add user routes to app.js for retrieving all users and a specific user by ID

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const controller = require('./controller');
 
 app.use(cors());
 
@@ -12,4 +13,19 @@ app.use(
 
 app.use(express.json());
 
-module.exports = app;
+app.get('/users', (req, res) => {
+    var resobj =[];
+    controller.getusers(users => {
+        res.send(users);
+    });
+
+})
+
+app.get('/user', (req, res) => {
+    const id = req.query.id;
+    controller.getUserByid(id, user => {
+        res.send(user); 
+    });
+});
+
+module.exports = app; 

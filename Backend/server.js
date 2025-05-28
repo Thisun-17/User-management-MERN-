79953a2580294a1dc8356343ece0649c577c3bd1
Thisun-17 <thisun.20233173@iit.ac.lk>
2025-05-28 22:17:48 +0530

    Refactor server.js to include Express and MongoDB setup with CORS support

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,25 @@
-const app = require('./app');
 const port = 3000;
 const host = 'localhost';
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const e = require('express');
+
+app.use(cors());
+app.use(express.json());
+
+const url = 'mongodb+srv://chanuka04:Anthony#Rb25@usermanage.nuspn93.mongodb.net/?retryWrites=true&w=majority&appName=UserManage';
+
+const connect = async () => {
+    try {
+        await mongoose.connect(url);
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+};
+connect();
 
 const server = app.listen(3000, () => {
     console.log('Server is running on port 3000');

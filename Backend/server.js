5cdf040585e23b51ec5479e79fa8336fccafc1f1
Thisun-17 <thisun.20233173@iit.ac.lk>
2025-05-29 01:14:14 +0530

    Update MongoDB connection URL and ensure API routes are properly set up

--- Backend/server.js
+++ Backend/server.js
@@ -5,11 +5,12 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const e = require('express');
+const { route } = require('./router');
 
 app.use(cors());
 app.use(express.json());
 
-const url = 'mongodb+srv://chanuka04:Anthony#Rb25@usermanage.nuspn93.mongodb.net/?retryWrites=true&w=majority&appName=UserManage';
+const url = 'mongodb+srv://chanuka17:AEnqm1XEIomGYZ1u@usermanage.nuspn93.mongodb.net/?retryWrites=true&w=majority&appName=UserManage';
 
 const connect = async () => {
     try {
@@ -24,3 +25,5 @@ connect();
 const server = app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
+
+app.use('/api',route);
